53f35949f210004fffcfa008b3d7eec31cd67cb7
Walysson Paiva <walyssonpaiva@outlook.com>
2022-01-23 21:19:44 -0300

    feat: index route

--- lib/app.py
+++ lib/app.py
@@ -5,5 +5,7 @@ from services import article as s_article
 app = FastAPI()
 app.include_router(s_article.router)
 
-
+@app.get("/", status_code=200)
+async def root():
+    return "Back-end Challenge 2021 🏅 - Space Flight News"
 
