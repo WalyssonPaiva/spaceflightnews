2cb7a1fb203e6ed9507ead5689fe3f72a7e17628
Walysson Paiva <walyssonpaiva@outlook.com>
2022-01-24 18:02:37 -0300

    fix: fix model attributes

--- lib/models/article.py
+++ lib/models/article.py
@@ -6,14 +6,13 @@ class Event(BaseModel):
     provider: str
 class Article(BaseModel):
     id: Optional[int] = None
-    featured: bool
-    provider: str
     title: str
-    description: str
     url: str
-    image: str
-    published: str
-    content: str
-    tags: list
+    imageUrl: str
+    newsSite: str
+    summary: str
+    publishedAt: str
+    updatedAt: str
+    featured: bool
     launches: Optional[List[Event]] = None
     events: Optional[List[Event]] = None
