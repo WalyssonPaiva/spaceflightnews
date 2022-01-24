135f84f742a64c28be3e0e1fc84cb2df41c3dfe4
Walysson Paiva <walyssonpaiva@outlook.com>
2022-01-24 18:01:40 -0300

    refactor: using .env variable for collections

--- lib/database/article.py
+++ lib/database/article.py
@@ -1,22 +1,29 @@
 from . import connection
+from decouple import config
+
 
 def create_article(article):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     collection.insert_one(article)
+    del article["_id"]
     return article
 
 def get_latest():
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     return collection.find().sort("_id", -1).limit(1)
 
 def get_next_id():
     last = get_latest()
-    if last== None:
-        return 1
-    return last[0]['id'] + 1
+    try:
+        if last== None:
+            return 1
+        return last[0]['id'] + 1
+    except:
+        return 0
+    
 
 def get_articles(page, per_page):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     items =  collection.find().sort("_id", -1).skip((page -1)* per_page).limit(per_page)
     response = []
     for item in items:
@@ -25,29 +32,33 @@ def get_articles(page, per_page):
     return response
 
 def get_article(id):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     item = collection.find_one({"id": id})
     del item["_id"]
     return item
 
 def update_article(id, article):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     collection.update_one({"id": id}, {"$set": article})
     return article
 
 def delete_article(id):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     collection.delete_one({"id": id})
     return id
 
 def insert_many(articles):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     collection.insert_many(articles)
 
 def get_by_title(title):
-    collection = connection.get_collection('articles')
+    collection = connection.get_collection(config('COLLECTION'))
     item = collection.find_one({"title": title})
     if item:
         del item["_id"]
         return item
     return None
+
+def delete_collection():
+    collection = connection.get_collection(config('COLLECTION'))
+    collection.drop()
