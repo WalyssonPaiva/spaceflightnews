bf0c4cf040de572541e6a76c994012c27947db65
Walysson Paiva <walyssonpaiva@outlook.com>
2022-01-24 18:02:49 -0300

    chore: remove unused import

--- lib/database/connection.py
+++ lib/database/connection.py
@@ -2,8 +2,6 @@ from decouple import config
 
 def get_database():
     from pymongo import MongoClient
-    import pymongo
-
     CONNECTION_STRING = config("DATABASE_CONNECTION")
     client = MongoClient(CONNECTION_STRING)
     return client['spaceflightnews']
